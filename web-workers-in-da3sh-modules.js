8e770922b76a1d908af003ee692690a0332ff421
Alan Lansdowne <daimyou@rounin.co.uk>
2020-01-09 11:39:04 +0000

    Create web-workers-in-da3sh-modules.js

---
+++ web-workers-in-da3sh-modules.js
@@ -0,0 +1,35 @@
+  //*************************************//
+ // DA3SH MODULES :: ENABLE WEB WORKERS //
+//*************************************//
+
+let ashivaModuleWorkers = {};
+
+function new_Worker (workerData) {
+
+  // 1) NEED TO FIGURE OUT HOW WORKERS CAN SPAWN SUB-WORKERS
+  // 2) LOOK INTO importScript('file1.js'); and self.importScript('file2.js', 'file3.js');
+  // 3) NEED TO DISTINGUISH BETWEEN DEDICATED WORKERS AND SHARED WORKERS
+  // 4) NEED TO FIGURE OUT HOW TO DEAL WITH new Worker('my-module.mjs', { type : module });
+  // 5) NEED TO LEARN ABOUT WORKLETS
+
+  let [workerName, ashivaModule, ashivaPublisher] = [workerData.workerName, workerData.ashivaModule, workerData.ashivaPublisher];
+  workerData.workerAddress = `${ashivaModule}»by»${ashivaPublisher}»»»${workerName}`;
+
+  getRemoteResponse(`/.assets/modules/${ashivaPublisher}/${ashivaModule}/datasheets/${workerName}on`, getNewWorker, workerData);
+}
+
+function getNewWorker (workerJSON, customObject) {
+
+  let [workerName, workerAddress, workerFunction] = [customObject.workerName, customObject.workerAddress, customObject.workerFunction];
+  
+  let workerScript = renderScript(workerJSON);
+  let workerBlob = new Blob([workerScript], {type: 'text/js-worker'});
+  let workerURL = window.URL.createObjectURL(workerBlob);
+
+  ashivaModuleWorkers[workerAddress][workerURL] = workerFunction;
+
+  let worker = new Worker(workerURL);
+  customObject.workerFunction(worker, customObject);
+}
+
+console.log(ashivaModuleWorkers);
